744190751b31e05663f77f107926c80702c2be0f
christophersesugh <christohybrid185@gmail.com>
2023-05-25 06:28:12 +0000

    Project initial setup

---
+++ monty.h
@@ -0,0 +1,60 @@
+#ifndef _MONTY_
+#define _MONTY_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+/**
+ * struct stack_s - doubly linked list for a stack or queue
+ * @n: integer
+ * @prev: points to the previous element of the stack or queue
+ * @next: points to the next element of the stack or queue
+ * Description: double linked list node structure for monty ALX project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
+ * struct globals - global structure for function use
+ * @lifo: stack or queue
+ * @cont: current line
+ * @arg: second parameter inside the current line
+ * @head: doubly linked list
+ * @fd: file descriptor
+ * @buffer: input text
+ * Description: doubly linked list node structure for ALX monty project
+ */
+typedef struct globals
+{
+	int lifo;
+	unsigned int cont;
+	char *arg;
+	stack_t *head;
+	FILE *fd;
+	char *buffer;
+} global_t;
+
+/**
+ * struct instruction_s - opcode functions
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ * Description: opcode and its function for ALX monty project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern global_t vglo;
+
+#endif /* _MONTY_ */
